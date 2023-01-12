db56303590cb4d8533a1c7236f16fe73a0673bab
Diego Salomon <diegosalomon@gmail.com>
2023-01-12 04:21:19 -0300

    Subiendo el proyecto final

---
+++ JS/funciones.js
@@ -0,0 +1,31 @@
+document.getElementById("Mostrar-Ocultar-detalles-explab").addEventListener('click', function () {
+    const detalle = document.getElementsByClassName("detalleLista");
+    mostrar(detalle, "Mostrar-Ocultar-detalles-explab");
+})
+document.getElementById("Mostrar-Ocultar-detalles-idioma").addEventListener('click', function () {
+    const detalle = document.getElementsByClassName("idioma-con-detalle");
+    mostrar(detalle, "Mostrar-Ocultar-detalles-idioma");
+})
+document.getElementById("Mostrar-Ocultar-detalles-cursos").addEventListener('click', function () {
+    const detalle = document.getElementsByClassName("detallecurso");
+    mostrar(detalle, "Mostrar-Ocultar-detalles-cursos");
+    moveTo: end
+})
+
+function mostrar(elements, boton) {
+
+    if (document.getElementById(boton).textContent == "mostrar Detalles") {
+        document.getElementById(boton).textContent = "Ocultar Detalles"
+        
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = "block"
+        }
+        document.getElementById(boton).style.filter= "brightness(80%)";
+    } else {
+        document.getElementById(boton).textContent = "mostrar Detalles"
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = "none";
+        }
+        document.getElementById(boton).style.filter= "brightness(100%)";
+    }
+}
